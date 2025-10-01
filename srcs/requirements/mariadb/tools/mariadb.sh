7439fb46e7478bceffb8e062541c5763cbdc8eed
2iaad <ziadderfoufi77@gmail.com>
2025-10-01 16:00:37 +0000

    [done with mariadb] not tested yet

---
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#mariadb start
+service mariadb start
+sleep 5
+
+#mariadb config
+mariadb -h localhost --execute="
+        CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`\g
+        CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'\g
+        GRANT ALL PRIVILEGES ON \`${MYSQL_DB}\`.* TO \`${MYSQL_USER}\`@'%'\g
+        FLUSH PRIVILEGES\g"
+
+#mariadb restart -> Shutdown mariadb to restart with new config
+mariadb -u root -p$MYSQL_ROOT_PASSWORD --execute="SHUTDOWN;"
+
+# Restart mariadb with new config in the background to keep the container running
+mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
