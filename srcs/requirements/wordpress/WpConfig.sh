4a259565ad180f2eab5951697a31040153b18ef7
2iaad <ziadderfoufi77@gmail.com>
2025-10-11 18:32:57 +0000

    [fixed the cross problem]

--- srcs/requirements/wordpress/WpConfig.sh
+++ srcs/requirements/wordpress/WpConfig.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-#---------------------------------------------------wp installation---------------------------------------------------#
+
 # Download WP-CLI
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-# wp-cli permission
-chmod +x wp-cli.phar
-# wp-cli move to bin
-mv wp-cli.phar /usr/local/bin/wp
-
-# Prepare directory
+curl    -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod   +x wp-cli.phar
+mv      wp-cli.phar /usr/local/bin/wp
 install --directory --mode=775 --owner=www-data --group=www-data /var/www/wordpress
-cd  /var/www/wordpress
+cd      /var/www/wordpress
 
-#---------------------------------------------------wp installation---------------------------------------------------#
-
+if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
 # Download WordPress core files
-wp  core download --allow-root
+    wp core download --allow-root
+
 # Create wp-config.php that specifies how to connect to the DB
-wp  core config --dbhost=mariadb:3306 \
+wp  core config --dbhost="mariadb::3306" \
                 --dbname="$MYSQL_DB" \
                 --dbuser="$MYSQL_USER" \
                 --dbpass="$MYSQL_PASSWORD" \
@@ -33,8 +29,8 @@ wp  user create "$WP_U_NAME" "$WP_U_EMAIL" \
                 --user_pass="$WP_U_PASS" \
                 --role="$WP_U_ROLE" \
                 --allow-root
+fi
 
-#---------------------------------------------------php config---------------------------------------------------#
 # change listen port from unix socket to 9000
 sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
 # create a directory for php-fpm
