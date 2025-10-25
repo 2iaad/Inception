0ae285468ddfc5b7675772f804d21f7c24e3ac83
Ziyad Derfoufi <zderfouf@e1r6p15.1337.ma>
2025-10-25 14:16:03 +0100

    [Intra push version]

--- srcs/requirements/wordpress/WpConfig.sh
+++ srcs/requirements/wordpress/WpConfig.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Download WP-CLI
 curl    -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod   +x wp-cli.phar
 mv      wp-cli.phar /usr/local/bin/wp
@@ -8,32 +7,30 @@ install --directory --mode=775 --owner=www-data --group=www-data /var/www/wordpr
 cd      /var/www/wordpress
 
 if [ ! -f "wp-config.php" ]; then
-#  installs core files; PHP files and folders that make the WordPress system work.
+
 wp core download --allow-root
 
-# Create wp-config.php that specifies how to connect wordpress to the DB
 wp  core config --dbhost="mariadb:3306" \
                 --dbname="$MYSQL_DB" \
                 --dbuser="$MYSQL_USER" \
                 --dbpass="$MYSQL_PASSWORD" \
                 --allow-root
-# Install WordPress (site + add admin user to the data base linked previously)
+
 wp  core install    --url="$DOMAIN_NAME" \
                     --title="$WP_TITLE" \
                     --admin_user="$WP_ADMIN_N" \
                     --admin_password="$WP_ADMIN_P" \
                     --admin_email="$WP_ADMIN_E" \
                     --allow-root
-# Add more users
+
 wp  user create "$WP_U_NAME" "$WP_U_EMAIL" \
                 --user_pass="$WP_U_PASS" \
                 --role="$WP_U_ROLE" \
                 --allow-root 
 fi
 
-# change listen port from unix socket to 9000
 sed -i 's|listen = /run/php/php8.2-fpm.sock|listen = 0.0.0.0:9000|' /etc/php/8.2/fpm/pool.d/www.conf
-# create a directory for php-fpm
+
 mkdir -p /run/php
-# start php-fpm service in the foreground to keep the container running
+
 /usr/sbin/php-fpm8.2 -F
